daad166d409422e08fd621220440235ab7d3644f
Philipp <phofmann200@gmail.com>
2026-03-05 10:20:23 +0100

    changed nginx config to use env variables and replaced them with envsubst in the init script

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -4,7 +4,9 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/ssl/private/inception.key \
     -out /etc/ssl/certs/inception.crt \
-    -subj "/CN=localhost"
+    -subj "/CN=${DOMAIN_NAME}"
+
+envsubst '${DOMAIN_NAME}' < /etc/nginx/templates/nginx.conf > /etc/nginx/sites-available/default
 
 # Start Nginx in the foreground
 exec nginx -g "daemon off;"
