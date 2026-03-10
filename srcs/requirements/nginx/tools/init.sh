1f575dde9e2dbcd3b1e737ecb3662ecc3e89b953
Philipp <phofmann200@gmail.com>
2026-03-10 17:01:27 +0100

    removed ssl certificate generattion from init script and added it to secrets

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Generate a self-signed SSL certificate for Nginx
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/ssl/private/inception.key \
-    -out /etc/ssl/certs/inception.crt \
-    -subj "/CN=${DOMAIN_NAME}"
-
 envsubst '${DOMAIN_NAME}' < /etc/nginx/templates/nginx.conf > /etc/nginx/sites-available/default
 
 # Start Nginx in the foreground
